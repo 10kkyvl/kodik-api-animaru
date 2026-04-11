6a87f35e387ed3709c0a0bed00ca78cffd3c0f9d
10kkyvl <ztkkre@gmail.com>
2026-04-11 13:40:33 +0300

    change import link to current repo and add client arg to mock

--- api/countries.go
+++ api/countries.go
@@ -1,15 +1,19 @@
 package api
 
 import (
-	"github.com/hikasami/kodik-api/client"
-	"github.com/hikasami/kodik-api/models"
+	"github.com/10kkyvl/kodik-api-animaru/client"
+	"github.com/10kkyvl/kodik-api-animaru/models"
 )
 
 // Countries выполняет запрос к эндпоинту /countries API Kodik с параметрами models.CountriesParams.
-func Countries(cp *models.CountriesParams) (*models.CountriesResponse, error) {
+func Countries(c *client.Client, cp *models.CountriesParams) (*models.CountriesResponse, error) {
 	var response models.CountriesResponse
-	params := cp.ToMap()
-	err := client.DefaultClient.DoRequest("GET", "/countries", params, &response)
+
+	var params map[string]string
+	if cp != nil {
+		params = cp.ToMap()
+	}
+	err := c.DoRequest("GET", "/countries", params, &response)
 	if err != nil {
 		return nil, err
 	}
